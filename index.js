c1a7e151586faef7aa4d66bc5501e4672a0a7cbc
mgrist <mgrist@angelo.edu>
2022-06-23 18:39:52 -0500

    Added comments to methods

--- index.js
+++ index.js
@@ -11,7 +11,18 @@ let Chapter = class {
     #CFID = "";;
     #CFTOKEN = "";;
 
-    // returns a promise with a success or error message
+    /****************************************************************************/
+    /* login(username, password) method                                         */
+    /*                                                                          */
+    /* The login method is passed a username and password to log into the ACM   */
+    /* Admin Panel, where student chapters usually check their roster and other */
+    /* club information.                                                        */
+    /*                                                                          */
+    /* After logging in, the student roster is preemptively loaded into the     */
+    /* memberList variable. This is so that methods can be called without having*/
+    /* to first call the loadMembers() method. However, if the user wants to    */
+    /* ensure the member list is up-to-date, loadMembers() must be called.      */
+    /****************************************************************************/
     async login(username, password) {
         // create new form data needed to log into the admin panel
         const form = new FormData();
@@ -38,7 +49,7 @@ let Chapter = class {
             if (this.#loggedIn) {
                 resolve("Successfully logged in");
             }
-            else if (this.#loadMembers(this.#CFID, this.#CFTOKEN) !== 0) {
+            else if (this.#loadMembers() !== 0) {
                 reject("ERROR while loading chapter roster.");
             }
             else {
@@ -50,16 +61,25 @@ let Chapter = class {
         return result;
     }
 
-    // parses and loads members into memberList variable
-    // loads as array of objects
-    async #loadMembers(CFID, CFTOKEN) {
+    /****************************************************************************/
+    /* loadMembers() method                                                     */
+    /*                                                                          */
+    /* The loadMembers() method simply updates the memberList with the most     */
+    /* recent roster data. This is to be used when recent changes have been made*/
+    /* to the roster, and you want to update the memberList data to ensure it's */
+    /* up-to-date.                                                              */
+    /*                                                                          */
+    /* NOTE: This function takes several (5 avg) seconds to execute due to the  */
+    /* load times of ACM servers.                                               */
+    /****************************************************************************/
+    async loadMembers() {
         // get request that returns the list of members in the chapter, in a csv format
         await axios
         .get(
             "https://services.acm.org/public/chapters/loadmembers/view_edit.cfm?CFID=" +
-                CFID +
+                this.#CFID +
                 "&CFTOKEN=" +
-                CFTOKEN +
+                this.#CFTOKEN +
                 "&mcsv=y&expfilter=x"
         )
         .then((res) => {
@@ -71,7 +91,7 @@ let Chapter = class {
             return -1;
         });
 
-        // using csv parse library to parse csv string
+        // parses and loads members into memberList variables
         // columns: true enables array of objects
         this.#memberList = parse(this.#memberList, { columns: true });
 
